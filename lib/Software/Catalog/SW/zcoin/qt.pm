198b83d9d72118b329a3036c9174f3ef7f4af349
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-14 19:02:48 +0700

    Implement get_release_note() and get_available_versions()

--- lib/Software/Catalog/SW/zcoin/qt.pm
+++ lib/Software/Catalog/SW/zcoin/qt.pm
@@ -48,6 +48,59 @@ sub canon2native_arch_map {
     },
 }
 
+sub get_available_versions {
+    my ($self, %args) = @_;
+
+    my $res = extract_from_url(
+        url => "https://github.com/zcoinofficial/zcoin/releases",
+        re  => qr!/zcoinofficial/zcoin/tree/([^"/]+)!,
+        all => 1,
+    );
+    return $res unless $res->[0] == 200;
+    # sort versions from earliest
+    $res->[2] = [ sort { $self->cmp_version($a, $b) }
+                      map { s/\Av//; $_ }
+                      @{$res->[2]}];
+    $res;
+}
+
+sub get_release_note {
+    require Mojo::DOM;
+
+    my ($self, %args) = @_;
+    my $format = $args{format} // 'text';
+
+    my $version = $args{version} // do {
+        my $res = $self->get_latest_version(%args);
+        return $res unless $res->[0] == 200;
+        $res->[2];
+    };
+
+    $version =~ s/\Av//;
+
+    my $url = "https://github.com/zcoinofficial/zcoin/releases/tag/v$version";
+    my $res = extract_from_url(
+      url => $url,
+      code => sub {
+          my %cargs = @_;
+          my $dom = Mojo::DOM->new($cargs{content});
+          my $html = $dom->at(".markdown-body")->content;
+
+          if ($html) {
+              if ($format eq 'html') {
+                  return [200, "OK", $html];
+              } else {
+                  require HTML::FormatText::Any;
+                  return [200, "OK",
+                          HTML::FormatText::Any::html2text(html => $html)->[2]];
+              }
+          } else {
+              return [543, "Cannot scrape release note text from $url"];
+          }
+      },
+  );
+}
+
 # version
 # arch
 sub get_download_url {
